041502be50b23c4b2c01a37aad12216fea717955
kimad <rizkiahmad60@gmail.com>
2020-01-06 14:05:23 +0700

    third commit

--- models/favorite.js
+++ models/favorite.js
@@ -1,20 +1,23 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const favorite = sequelize.define('favorite', {
-    event_id: DataTypes.INTEGER,
-    user_id: DataTypes.INTEGER,
-    status: DataTypes.STRING
-  }, {});
+  const favorite = sequelize.define(
+    "favorite",
+    {
+      event_id: DataTypes.INTEGER,
+      user_id: DataTypes.INTEGER
+    },
+    {}
+  );
   favorite.associate = function(models) {
     // associations can be defined here
     favorite.belongsTo(models.user, {
       foreignKey: "user_id",
       as: "Users"
     }),
-    favorite.belongsTo(models.event, {
-      foreignKey: "event_id",
-      as: "Events"
-    })
+      favorite.belongsTo(models.event, {
+        foreignKey: "event_id",
+        as: "Events"
+      });
   };
   return favorite;
 };
