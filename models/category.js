05ceb09495e393dc0d58b6971724bdd823c8238b
kimad <rizkiahmad60@gmail.com>
2020-01-07 12:59:08 +0700

    third commit

--- models/category.js
+++ models/category.js
@@ -1,7 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const category = sequelize.define('category', {
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    image: DataTypes.STRING
   }, {});
   category.associate = function(models) {
     // associations can be defined here
